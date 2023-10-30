aa708f8c1e24ae5ad1a59d0adb49af5e1d1abf2c
Justin Choi <justinchoi244@gmail.com>
2023-10-30 14:46:49 -0500

    Exceptions exercises

--- src/util/Input.java
+++ src/util/Input.java
@@ -13,21 +13,33 @@ public class Input {
         System.out.print("Enter yes or no: " );
         return scanner.next().equalsIgnoreCase("yes");
     }
-    int getInt(int min, int max) {
+    int getInt(int min, int max) throws NumberFormatException {
         System.out.println("Enter a number between 1 and 10: ");
-        int userInput = scanner.nextInt();
-        if (userInput >= min && userInput <= max) {
-            return userInput;
-        } else {
+        try {
+            String userInput = scanner.next();
+            int intInput = Integer.parseInt(userInput);
+            if (intInput >= min && intInput <= max) {
+                return intInput;
+            } else {
+                return getInt(min, max);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Input is not a number;");
             return getInt(min, max);
         }
     }
     double getDouble(int min, int max) {
         System.out.println("Enter a number between 1 and 10: ");
-        double userInput = scanner.nextDouble();
-        if (userInput >= min && userInput <= max) {
-            return userInput;
-        } else {
+        String userInput = scanner.next();
+        try {
+            double doubleInput = Double.parseDouble(userInput);
+            if (doubleInput >= min && doubleInput <= max) {
+                return doubleInput;
+            } else {
+                return getDouble(min, max);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Input is not a number;");
             return getDouble(min, max);
         }
     }
